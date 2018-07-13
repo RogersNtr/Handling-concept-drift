f201e4594f671752ebf33dde6ef0d4321c2cb288
Roger Nengwe <r.nengwe@yahoo.fr>
2018-07-13 16:18:00 +0100

    Adding ADWIN and kolmogorov_smirnov

--- drift_test_scanning_window.py
+++ drift_test_scanning_window.py
@@ -68,17 +68,20 @@ def remove_outlier2(input_data, label_type=None):
     return input_data_copy
 
 
-def ADWIN(datastream, confidence_interval=None):
+def ADWIN(datastream, confidence_interval=None, window_size=None):
     """
-
+    The function implements the ADWIN  algorithm
     :param datastream: The datastream of Examples
     :param confidence_interval: The level of confidence to the detection made
     :return: W, a window of examples
     """
+    if confidence_interval is None:
+        confidence_interval = 2
     # Initialize the Window
     height = datastream.shape[0]
     rand = np.random.randint(1, 52)
-    rand = 2
+    rand = 100
+
     W = datastream[1:rand]
     for xi in range(rand, datastream.shape[0]):
         df2 = pd.DataFrame([[xi + 1, "GISTEMP", "2012-10-27", datastream['Mean'][xi], rand + 1]], columns=["Unnamed: 0", "Source", "Date", "Mean", "index"])
@@ -88,16 +91,44 @@ def ADWIN(datastream, confidence_interval=None):
         W0 = W
         W1 = datastream[xi:height]
 
-        # Compute the avarage
+        # Compute the average
         mean_W0_hat = np.mean(W0)
         mean_W1_hat = np.mean(W1)
 
-        #
-        
+        # Calculate epsilon
+        n0 = (W0.shape[0] )*( W0.shape[1])
+        n1 = (W1.shape[0] )*( W1.shape[1])
+        n = n0 + n1
+        m = 1 / (1/n0 + 1/n1)
+        sigmap = confidence_interval / n
+        epsilon = np.sqrt((1/2*m) * (4/sigmap))
+        print('epsilon', epsilon)
+        diff = np.absolute(mean_W0_hat - mean_W1_hat)
+        print(diff)
+        while diff < epsilon:
+            W.drop([W.shape[0] - 1])
+
+    return W
 
 
+def kolmogorov_smirnov(data1, data2, window_size):
+    """
+    The function is the Kolmogorov smirnov test, that use the
+    :param data: Column vector
+    :param window_size: Size of the Scanning Window
+    :return: True, False (True : Drift Present, False : Drift Absent)
+    """
+    # W0 = data[1:window_size]
+    # for t in range(len(data)):
+    #     W2 = data[window_size - t + 1:window_size]
+    result = stats.ks_2samp(data1, data2)
+    if result == 0:
+        drift = True
+    else:
+        drift = False
 
-    return True
+    return drift
+
 
 
 if __name__ == '__main__':
@@ -125,20 +156,20 @@ if __name__ == '__main__':
     # plt.plot(result['Mean'])
     # plt.show()
     # # print(data2)
-    # result_GCAG = remove_outlier(data_GCAG, 'GCAG')
-    # result_GISTEMP = remove_outlier(data_GISTEMP, 'GISTEMP')
+    result_GCAG = remove_outlier(data_GCAG, 'GCAG')
+    result_GISTEMP = remove_outlier(data_GISTEMP, 'GISTEMP')
     # # print(result)
     # # GCAG, data
-    # data_mean_GCAG = data_GCAG['Mean'][result_GCAG]
-    # data_date_GCAG = data_GCAG['Date'][result_GCAG]
+    data_mean_GCAG = data_GCAG['Mean'][result_GCAG]
+    data_date_GCAG = data_GCAG['Date'][result_GCAG]
     #
     # # GISTEMP data
-    # data_mean_GISTEMP = data_GISTEMP['Mean'][result_GISTEMP]
-    # data_date_GISTEMP = data_GISTEMP['Date'][result_GISTEMP]
+    data_mean_GISTEMP = data_GISTEMP['Mean'][result_GISTEMP]
+    data_date_GISTEMP = data_GISTEMP['Date'][result_GISTEMP]
     # print(data_mean)
     # Plotting to see how it looks.
-    # plt.figure(1)
-    # plt.plot(data_date_GCAG, data_mean_GCAG)
+    #plt.figure(1)
+    #plt.plot(data_date_GCAG, data_mean_GCAG)
     # plt.show()
     # plt.pause(10)
     # plt.figure(2)
@@ -146,6 +177,7 @@ if __name__ == '__main__':
     # plt.show()
 
     x = np.linspace(-np.pi, np.pi, 644)
+
     # mean_data = stats.mean(x)  # Mean value of the data
     # var_data = stats.variance(x)  # Variance of the data
     # A = []
@@ -155,19 +187,37 @@ if __name__ == '__main__':
     #
     # print(len(A))
 
-    sin_template = pd.DataFrame(x, columns=['Mean'])
-    # result.append(sin_template)
-    # result.to_csv('test_df_csv.csv')
-    print(result)
-    # sin_template = remove_outlier2(sin_template)
-    #print(sin_template)
     result.to_csv('test_df_csv.csv')
     data3 = pd.read_csv('test_df_csv.csv')
-    print(data3[1:100])
 
-    print('random', np.random.randint(1, 51))
+    sin_template = pd.DataFrame(x, columns=['Mean'])
+    date_add = ["2012-11-27" for j in range(sin_template.shape[0])]
+    origin_add = ["GISTEMP" for j in range(sin_template.shape[0])]
+    unnamed_add = [(data3.shape[0]) + j for j in range(sin_template.shape[0])]
+    index_add = [(data3.shape[0]) + j for j in range(sin_template.shape[0])]
+    h = np.vstack([[unnamed_add[j],  origin_add[j], date_add[j], sin_template['Mean'][j], index_add[j]] for j in range(sin_template.shape[0])])
+    h_pd = pd.DataFrame(h, columns=['Unnamed: 0', 'Source', 'Date','Mean','index'])
+    result.append(h_pd)
 
-    #plt.plot(x, np.sin(4*np.pi*x))
-    #plt.show()
+    #plt.plot(result)
+    # result.to_csv('test_df_csv.csv')
+    result_ADWIN = ADWIN(result)
+
+    print(result)
+    plt.figure(2)
+    plt.plot(result['Mean'])
+    plt.show()
+
+    # Testing ADWIN
+
+    # sin_template = remove_outlier2(sin_template)
+    #print(sin_template)
+
+    # print(data3)
+
+    # print('random', np.random.randint(1, 51))
+    # plt.figure()
+    # plt.plot(x, np.sin(4*np.pi*x))
+    # plt.show()
 
 
