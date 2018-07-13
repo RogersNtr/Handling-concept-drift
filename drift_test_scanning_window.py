2782bb7912203a23e13802f83e533bf3ae3fff21
Roger Nengwe <r.nengwe@yahoo.fr>
2018-07-13 18:32:09 +0100

    Remove outlier Fix, consider only remove outliers

--- drift_test_scanning_window.py
+++ drift_test_scanning_window.py
@@ -7,70 +7,65 @@ import statistics as stats
 
 
 def load_data(filename):
-    ''' Load a file, given its name.
+    """ Load a file, given its name.
     filename-- name of the file we want to open.
 
-    :return dico
-    '''
+    :return a list containing all rows
+    """
     result = []
     dico={}
     line_i=[]
     with open(filename, 'r') as joint_prob_file:
-        # print(joint_prob_file.readlines())
         joint_prob_file.readline()
-        #result = joint_prob_file.readlines()
         for line in joint_prob_file.readlines():
             line_i = line.split()
             line_i = [float(string.strip()) for string in line_i]
             result.append(line_i)
-            # print(line)
-            # line_i = line.split(',')
-            # line_i = [string.strip() for string in line_i]  # remove white space and special character
-            # dico[(line_i[0], line_i[1])] = float(line_i[2])
     return result
 
 
 # Preprocessing
-def remove_outlier(input_data, label_type):
-    mean_data = stats.mean(input_data['Mean'])  # Mean value of the data
-    var_data = stats.variance(input_data['Mean'])  # Variance of the data
-    output_data = [] #input_data['Mean'][mean_data - 3*var_data < input_data['Mean'] < mean_data + 3*var_data]
-    count = 0
+def remove_outlier(input_data, label_type=None):
+    """
+    :param input_data: the datastream in which we remove outliers
+    :param label_type: The type of label (Source) of the data (GCAG, GISTEMP)
+    :return: indices, of the inliers values.
+    """
+    mean_data = np.mean(input_data['Mean'])  # Mean value of the data
+    var_data = np.std(input_data['Mean'])  # Variance of the data
+    output_data = [] # return the indices of the inliers values
     for j in range(len(input_data['Mean'])):
         if label_type == 'GCAG':
-            if mean_data + 4*var_data > input_data['Mean'][2 * j] > mean_data - 4*var_data:
-                # print("test", input_data['Mean'][2*j])
-                count = count + 1
-                # print(2*j)
+            if mean_data + 3*var_data > input_data['Mean'][2 * j] > mean_data - 3*var_data:
                 output_data.append(2*j)
         else:
-            if mean_data + 4*var_data > input_data['Mean'][2 * j + 1] > mean_data - 4*var_data:
-                # print("test", input_data['Mean'][2*j])
-                count = count + 1
-                # print(2*j)
+            if mean_data + 3*var_data > input_data['Mean'][2 * j + 1] > mean_data - 3*var_data:
                 output_data.append(2*j + 1)
-    print(count)
-    print(len(input_data['Mean']))
     return output_data
 
+
 # remove_outlier 2
-def remove_outlier2(input_data, label_type=None):
-    mean_data = stats.mean(input_data['Mean'])  # Mean value of the data
-    var_data = stats.variance(input_data['Mean'])  # Variance of the data
-    output_data = [] #input_data['Mean'][mean_data - 3*var_data < input_data['Mean'] < mean_data + 3*var_data]
-    count = 0
+def remove_outlier2(input_data):
+    """
+    The function remove outliers to the input datastream.
+    :param input_data: the datastream
+    :return: the datastream, with remove outliers
+    """
+    mean_data = np.mean(input_data['Mean'])  # Mean value of the data
+    var_data = np.std(input_data['Mean'])  # Variance of the data
     input_data_copy = input_data.copy()
+    a = []
     for j in range(len(input_data_copy['Mean'])):
-        if mean_data + 4*var_data > input_data_copy['Mean'][j] > mean_data - 4*var_data:
-            # print("test", input_data['Mean'][2*j])
-            input_data_copy = input_data_copy.drop([j])
-    print(len(input_data['Mean']))
+        if mean_data - 4*var_data < input_data_copy['Mean'][j] < mean_data + 4*var_data:
+            # a.append(j)
+            input_data_copy = input_data_copy.drop([j]) # Remove the corresponding rows
+    # A = input_data_copy[a]
     return input_data_copy
 
 
-def ADWIN(datastream, confidence_interval=None, window_size=None):
+def ADWIN(datastream, confidence_interval=None):
     """
-    The function implements the ADWIN  algorithm
+    The function implements the ADWIN  algorithm, for Drift detection
     :param datastream: The datastream of Examples
     :param confidence_interval: The level of confidence to the detection made
     :return: W, a window of examples
@@ -130,83 +125,94 @@ def kolmogorov_smirnov(data1, data2, window_size):
     return drift
 
 
-
 if __name__ == '__main__':
     filename = 'data_set_test_weather/monthly_csv_temp.csv'
-    data = pd.DataFrame.from_csv(filename)
+    # data = pd.DataFrame.from_csv(filename)
     data2 = pd.read_csv(filename)
 
-
-    # print(data2)
+    # Original data
+    # ---Mean
     data_GCAG = data2[data2['Source'] == 'GCAG']
     data_GISTEMP = data2[data2['Source'] == 'GISTEMP']
-    print(type(data_GCAG['Date'][0]))
+
+    # --Date
     date_GCAG = data_GCAG['Date']
     date_GISTEMP = data_GISTEMP['Date']
-    # print(date_GCAG)
+
     # for j in range(len(date_GCAG)):
     #     data_GCAG['Date'][2*j] = j
     #     data_GISTEMP['Date'][2*j + 1] = j
+
+    # Remove outlier and add modify indexes
     result = remove_outlier2(data2)
+    print("rmv_outlier 2", result.shape)
     a = [j for j in range(result.shape[0])]
     result['index'] = a
 
+    # Plotting the original data
     # print(result)
-    # plt.figure()
-    # plt.plot(result['Mean'])
-    # plt.show()
+    plt.figure(1)
+    plt.plot(result['Mean'])
+    plt.title("Original Data with outliers removes")
+    plt.ylabel("Mean temperature distribution")
+    plt.draw()
+    plt.pause(20)
+
     # # print(data2)
     result_GCAG = remove_outlier(data_GCAG, 'GCAG')
     result_GISTEMP = remove_outlier(data_GISTEMP, 'GISTEMP')
-    # # print(result)
-    # # GCAG, data
+
+    # # # GCAG data
     data_mean_GCAG = data_GCAG['Mean'][result_GCAG]
     data_date_GCAG = data_GCAG['Date'][result_GCAG]
-    #
-    # # GISTEMP data
+    print("rmv_outlier", data_mean_GCAG.shape)
+
+    # # # GISTEMP data
     data_mean_GISTEMP = data_GISTEMP['Mean'][result_GISTEMP]
     data_date_GISTEMP = data_GISTEMP['Date'][result_GISTEMP]
-    # print(data_mean)
-    # Plotting to see how it looks.
-    #plt.figure(1)
-    #plt.plot(data_date_GCAG, data_mean_GCAG)
-    # plt.show()
-    # plt.pause(10)
-    # plt.figure(2)
-    # plt.plot(data_date_GISTEMP, data_mean_GISTEMP)
-    # plt.show()
 
-    x = np.linspace(-np.pi, np.pi, 644)
-
-    # mean_data = stats.mean(x)  # Mean value of the data
-    # var_data = stats.variance(x)  # Variance of the data
-    # A = []
-    # for val in x:
-    #     if mean_data - 2*var_data < val < mean_data + 2*var_data:
-    #         A.append(val)
-    #
-    # print(len(A))
-
-    result.to_csv('test_df_csv.csv')
-    data3 = pd.read_csv('test_df_csv.csv')
-
-    sin_template = pd.DataFrame(x, columns=['Mean'])
-    date_add = ["2012-11-27" for j in range(sin_template.shape[0])]
-    origin_add = ["GISTEMP" for j in range(sin_template.shape[0])]
-    unnamed_add = [(data3.shape[0]) + j for j in range(sin_template.shape[0])]
-    index_add = [(data3.shape[0]) + j for j in range(sin_template.shape[0])]
-    h = np.vstack([[unnamed_add[j],  origin_add[j], date_add[j], sin_template['Mean'][j], index_add[j]] for j in range(sin_template.shape[0])])
-    h_pd = pd.DataFrame(h, columns=['Unnamed: 0', 'Source', 'Date','Mean','index'])
-    result.append(h_pd)
-
-    #plt.plot(result)
-    # result.to_csv('test_df_csv.csv')
-    result_ADWIN = ADWIN(result)
-
-    print(result)
+    # # Plotting to see how it looks.
     plt.figure(2)
-    plt.plot(result['Mean'])
-    plt.show()
+    plt.plot(data_mean_GCAG)
+    plt.title("Test")
+    plt.draw()
+    plt.pause(20)
+    # # plt.pause(10)
+    # # plt.figure(2)
+    # # plt.plot(data_date_GISTEMP, data_mean_GISTEMP)
+    # # plt.show()
+    #
+    # x = np.linspace(-np.pi, np.pi, 644)
+    #
+    # # mean_data = stats.mean(x)  # Mean value of the data
+    # # var_data = stats.variance(x)  # Variance of the data
+    # # A = []
+    # # for val in x:
+    # #     if mean_data - 2*var_data < val < mean_data + 2*var_data:
+    # #         A.append(val)
+    # #
+    # # print(len(A))
+    #
+    # result.to_csv('test_df_csv.csv')
+    # data3 = pd.read_csv('test_df_csv.csv')
+    #
+    # sin_template = pd.DataFrame(x, columns=['Mean'])
+    # date_add = ["2012-11-27" for j in range(sin_template.shape[0])]
+    # origin_add = ["GISTEMP" for j in range(sin_template.shape[0])]
+    # unnamed_add = [(data3.shape[0]) + j for j in range(sin_template.shape[0])]
+    # index_add = [(data3.shape[0]) + j for j in range(sin_template.shape[0])]
+    # h = np.vstack([[unnamed_add[j],  origin_add[j], date_add[j], sin_template['Mean'][j], index_add[j]] for j in range(sin_template.shape[0])])
+    # h_pd = pd.DataFrame(h, columns=['Unnamed: 0', 'Source', 'Date','Mean','index'])
+    # result.append(h_pd)
+    #
+    # #plt.plot(result)
+    # # result.to_csv('test_df_csv.csv')
+    # result_ADWIN = ADWIN(result)
+    #
+    # print(result)
+    # plt.figure(2)
+    # plt.plot(result['Mean'])
+    # plt.show()
 
     # Testing ADWIN
 
