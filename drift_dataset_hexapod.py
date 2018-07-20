b55431f75dae9c03651dbdc83ab9ff4d642602ca
Roger Nengwe <r.nengwe@yahoo.fr>
2018-07-20 02:00:01 +0100

    Testing possiblities of concatenation of the dataset

--- drift_dataset_hexapod.py
+++ drift_dataset_hexapod.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy as sc
 import pandas as pd
 import matplotlib.pyplot  as plt
+from datetime import datetime
+
 
 def load_data(filename):
     ''' Load a file, given its name.
@@ -50,6 +52,46 @@ def readfile_pow(file_ref):
             a = np.array([tt_i, V_i, I_i])  # ,dtype=DTYPE)
             pow = np.vstack([pow, a])
     return pow
+
+
+def concatenate_data(data1, data2, window_size=1024):
+    """
+    Concatenate two data
+    :param data1: data to concatenate
+    :param data2: second data to concatenate
+    :param window_size: 1024
+    :return: the data already concatenate
+    """
+    data_concatenate = True
+    return data_concatenate
+
+
+def norm_(x, min_, max_):
+    return (x-min_)/(max_ - min_)
+
+
+def normalize(datastream):
+    """
+    Normalize the data stream to [0,1]
+    :param datastream: The data stream to normalize
+    :return: The normalized data stream
+    """
+    min_ = np.min(datastream['current'])
+    max_ = np.max(datastream['current'])
+    for index, row in datastream.iterrows():
+        datastream = datastream.replace(row['current'], norm_(row['current'], min_, max_))
+    # datastream['current'].apply(norm_)
+    return datastream
+
+
+def remove_outlier(datastream, normalized=None):
+    """
+    Remove outlier in the current  normalized datastream
+    :param datastream: The datastream to clean
+    :return: The Clean Dataset with remove outlier (points outside [mean - 3*sigma, mean + 3*sigma]
+    """
+
+
 if __name__ == '__main__':
     filepath_bf = 'Hexapod_dataset/black_flat/f_b_1.pow'  # Black_flat
     filepath_br = 'Hexapod_dataset/black_rough/t_b_1.pow'  # Black_rough
@@ -93,15 +135,41 @@ if __name__ == '__main__':
     elt_gf = 3*np.ones(shape_nber_gf, dtype=np.int8)
     elt_gr = 4*np.ones(shape_nber_gr, dtype=np.int8)
 
+    current_bf_pd['label'] = zero_elt
     current_br_pd['label'] = one_elt_br
     current_cu_pd['label'] = elt_cu
     current_gf_pd['label'] = elt_gf
     current_gr_pd['label'] = elt_gr
 
     # Normalization of the current between [0, 1]
-    current_bf_pd.set_index(['current', 'label'])
+    window_size = 10
+    A = current_bf_pd[1:window_size]
+    B = current_br_pd[1:window_size]
+    # for index, row in A.iterrows():
+    #     print(row['current'])
+    # print(A)
+    frames = []
+    start = datetime.now()
+    for i in range(0, 100, window_size):
+        A = current_bf_pd[i:i+window_size]
+        B = current_br_pd[i:i+window_size]
+        # print(type(A))
+        frames.append(A)
+        smooth_A = [{'current':(current_bf_pd['current'][i-1+window_size] + current_br_pd['current'][i])/2, 'label':-1}]
+        smooth_A = pd.DataFrame(smooth_A)
+        # print(type(smooth_A))
+        # dicts = [{'current':}, {}]
+        frames.append(smooth_A)
+        frames.append(B)
+    C = pd.concat(frames, ignore_index=True)
+    end = datetime.now() - start
+    # print("start : {}, end : {} ".format(start, end))
+    # print(C)
+    # current_bf_pd.set_index(['current', 'label'])
+    D = normalize(C)
+    print(D)
 
-    print(np.max(current_br_pd['current']))
+    # print(np.max(current_br_pd['current']))
     # print(current_cu_pd)
     #print(current_gf_pd)
     #print(current_gr_pd)
